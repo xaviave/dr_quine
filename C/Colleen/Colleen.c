f3ac3a3d1bbd025c4a1f4a050e33854c01eb7497
x.martin <xavier.martin@itsfactory.fr>
2020-06-01 18:53:25 +0200

    lil fix

--- C/Colleen/Colleen.c
+++ C/Colleen/Colleen.c
@@ -14,7 +14,7 @@ int			main()
 	/*
 		Smokin' on cookie in the hotbox
 	*/
-	char	*s1 = "#include <stdio.h>%1$c%1$c/*%1$c%2$cRaindrops%1$c*/%1$c%1$cint%2$c%2$c%2$cdrop_tops()%1$c{%1$c%2$creturn (0);%1$c}%1$c%1$cint%2$c%2$c%2$cmain()%1$c{%1$c%2$c/*%1$c%2$c%2$cSmokin' on cookie in the hotbox%1$c%2$c*/%1$c%2$cchar%2$c*s1 = %3$c%4$s%3$c;%1$c%1$c%2$cdrop_tops();%1$c%2$cprintf(s1, 10, 9, 34, s1);%1$c%2$creturn (0);%1$c}%1$c";
+	static char	*s1 = "#include <stdio.h>%1$c%1$c/*%1$c%2$cRaindrops%1$c*/%1$c%1$cint%2$c%2$c%2$cdrop_tops()%1$c{%1$c%2$creturn (0);%1$c}%1$c%1$cint%2$c%2$c%2$cmain()%1$c{%1$c%2$c/*%1$c%2$c%2$cSmokin' on cookie in the hotbox%1$c%2$c*/%1$c%2$cstatic char%2$c*s1 = %3$c%4$s%3$c;%1$c%1$c%2$cdrop_tops();%1$c%2$cprintf(s1, 10, 9, 34, s1);%1$c%2$creturn (0);%1$c}%1$c";
 
 	drop_tops();
 	printf(s1, 10, 9, 34, s1);
--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -4,8 +4,8 @@
 
 #define NAME "Sully_%d.c"
 #define COMPILE "gcc -Wall -Wextra -Werror %s -o Sully_%d ; ./Sully_%d"
-#define FORMAT "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <unistd.h>%1$c%1$c#define NAME %2$c%3$s%2$c%1$c#define COMPILE %2$c%4$s%2$c%1$c#define FORMAT %2$c%5$s%2$c%1$c#define PRINT(e,i) FILE *f=fopen(e,%2$cw%2$c);if(!f){free(e);exit(0);}fprintf(f,FORMAT,10,34,NAME,COMPILE,FORMAT,i);fclose(f);%1$c#define START() int main(){int i=%6$d;char *e, *g;if(i<1)return(0);if(access(%2$cSully_5.c%2$c,F_OK) != -1)i--;asprintf(&e,NAME,i);PRINT(e,i);asprintf(&g,COMPILE,e,i,i);system(g);free(e);free(g);}%1$c%1$cSTART()%1$c"
+#define FORMAT "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <unistd.h>%1$c%1$c#define NAME %2$c%3$s%2$c%1$c#define COMPILE %2$c%4$s%2$c%1$c#define FORMAT %2$c%5$s%2$c%1$c#define PRINT(e,i) FILE *f=fopen(e,%2$cw%2$c);if(!f){free(e);exit(0);}fprintf(f,FORMAT,10,34,NAME,COMPILE,FORMAT,i);fclose(f);%1$c#define START() int main(){int i=%6$d;char *e, *g;if(i<1)return(0);if(access(%2$cSully_5.c%2$c,F_OK) != -1)i--;asprintf(&e,NAME,i);PRINT(e,i);asprintf(&g,COMPILE,e,i,i);system(g);free(e);free(g);return(0);}%1$c%1$cSTART()%1$c"
 #define PRINT(e,i) FILE *f=fopen(e,"w");if(!f){free(e);exit(0);}fprintf(f,FORMAT,10,34,NAME,COMPILE,FORMAT,i);fclose(f);
-#define START() int main(){int i=5;char *e, *g;if(i<1)return(0);if(access("Sully_5.c",F_OK) != -1)i--;asprintf(&e,NAME,i);PRINT(e,i);asprintf(&g,COMPILE,e,i,i);system(g);free(e);free(g);}
+#define START() int main(){int i=5;char *e, *g;if(i<1)return(0);if(access("Sully_5.c",F_OK) != -1)i--;asprintf(&e,NAME,i);PRINT(e,i);asprintf(&g,COMPILE,e,i,i);system(g);free(e);free(g);return(0);}
 
 START()
